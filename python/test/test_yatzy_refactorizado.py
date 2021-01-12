1c4665a7ff37dfd1b1b31766bb5fc83a7fe4b5d2
gasparnovel <gnovel@cifpfbmoll.eu>
2021-01-12 23:47:01 +0100

    feat: :sparkles: refactor assert test_sixes_test

--- python/test/test_yatzy_refactorizado.py
+++ python/test/test_yatzy_refactorizado.py
@@ -46,9 +46,9 @@ def test_fives():
 
 
 def test_sixes_test():
-        assert 0 == Yatzy(4,4,4,5,5).sixes()
-        assert 6 == Yatzy(4,4,6,5,5).sixes()
-        assert 18 == Yatzy(6,5,6,6,5).sixes()
+        assert 0 == Yatzy.sixes(4,4,4,5,5) # refactor code
+        assert 6 == Yatzy.sixes(4,4,6,5,5) # refactor code
+        assert 18 == Yatzy.sixes(6,5,6,6,5) # refactor code
 
 
 def test_one_pair():
