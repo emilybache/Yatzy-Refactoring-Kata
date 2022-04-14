951f95ca1cad8d5db2ed9d5c60e2b4b6065c7770
Yoan Thirion <yoan.thirion@gmail.com>
2022-04-14 08:43:15 +0200

    dummy encapsulation

--- scala/src/main/scala/Yatzy/Yatzy.scala
+++ scala/src/main/scala/Yatzy/Yatzy.scala
@@ -3,11 +3,11 @@ package Yatzy
 class Yatzy(
     private val d1: Int,
     private val d2: Int,
-    private val d3: Int,
+    val d3: Int,
     private val d4: Int,
-    private val d5: Int
+    val d5: Int
 ) {
-  private val dice: List[Int] = List(d1, d2, d3, d4, d5)
+  val dice: List[Int] = List(d1, d2, d3, d4, d5)
 
   def fives(): Int = {
     var s = 0
