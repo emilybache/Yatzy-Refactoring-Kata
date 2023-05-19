4ee7fd16731524b99feebc10fcedd8ffa3dc0749
Vova Gromozdin <74236891+pioppka@users.noreply.github.com>
2023-05-19 20:44:29 +0300

    Update yatzy.hpp

--- cpp/src/yatzy.hpp
+++ cpp/src/yatzy.hpp
@@ -3,31 +3,21 @@
 
 class Yatzy
 {
-public:
+    protected:
+        int * dice;
+    public:
+        
+        Yatzy();
+        Yatzy(int d1, int d2, int d3, int d4, int _5);
+        
+        int getDiceValueSum(int diceValue);
+        int ScorePair(int d1, int d2, int d3, int d4, int d5);
+        int TwoPair(int d1, int d2, int d3, int d4, int d5);
+        int QuantityOfAKind(int quantity);
 
-    static int Chance(int d1, int d2, int d3, int d4, int d5);
-    static int yatzy(int dice[]);
-    static int Ones(int d1, int d2, int d3, int d4, int d5);
-    static int Twos(int d1, int d2, int d3, int d4, int d5);
-    static int Threes(int d1, int d2, int d3, int d4, int d5);
-
-protected:
-    int * dice;
-public:
-    Yatzy();
-    Yatzy(int d1, int d2, int d3, int d4, int _5);
-    int Fours();
-    int Fives();
-    int sixes();
-    int ScorePair(int d1, int d2, int d3, int d4, int d5);
-    static int TwoPair(int d1, int d2, int d3, int d4, int d5);
-    static int FourOfAKind(int _1, int _2, int d3, int d4, int d5);
-    static int ThreeOfAKind(int d1, int d2, int d3, int d4, int d5);
-
-    static int SmallStraight(int d1, int d2, int d3, int d4, int d5);
-    static int LargeStraight(int d1, int d2, int d3, int d4, int d5);
-    static int FullHouse(int d1, int d2, int d3, int d4, int d5);
-
+        int SmallStraight(int d1, int d2, int d3, int d4, int d5);
+        int LargeStraight(int d1, int d2, int d3, int d4, int d5);
+        int FullHouse(int d1, int d2, int d3, int d4, int d5);
 };
 
 #endif
