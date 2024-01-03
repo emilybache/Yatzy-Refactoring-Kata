bdb93f2fc8e5e8fa7d3a126ee388f34dedc62bab
Adrien Wehrung <a.wehrung@finatix.de>
2024-01-03 15:09:25 +0100

    kotlin: add missing negative test cases

--- kotlin/src/test/kotlin/YatzyTest.kt
+++ kotlin/src/test/kotlin/YatzyTest.kt
@@ -65,14 +65,17 @@ class YatzyTest {
     @Test
     fun one_pair() {
         assertEquals(6, Yatzy.score_pair(3, 4, 3, 5, 6))
+        assertEquals(6, Yatzy.score_pair(1, 3, 3, 3, 1))
         assertEquals(10, Yatzy.score_pair(5, 3, 3, 3, 5))
         assertEquals(12, Yatzy.score_pair(5, 3, 6, 6, 5))
+        assertEquals(0, Yatzy.score_pair(3, 4, 1, 5, 6))
     }
 
     @Test
     fun two_Pair() {
         assertEquals(16, Yatzy.two_pair(3, 3, 5, 4, 5))
         assertEquals(16, Yatzy.two_pair(3, 3, 5, 5, 5))
+        assertEquals(0, Yatzy.two_pair(3, 3, 2, 4, 5))
     }
 
     @Test
@@ -80,6 +83,7 @@ class YatzyTest {
         assertEquals(9, Yatzy.three_of_a_kind(3, 3, 3, 4, 5))
         assertEquals(15, Yatzy.three_of_a_kind(5, 3, 5, 4, 5))
         assertEquals(9, Yatzy.three_of_a_kind(3, 3, 3, 3, 5))
+        assertEquals(0, Yatzy.three_of_a_kind(3, 1, 3, 4, 5))
     }
 
     @Test
