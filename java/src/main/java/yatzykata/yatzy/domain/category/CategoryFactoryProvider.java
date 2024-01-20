7d3dce6ed17ed1a2268b66db99c0e96b170ad02c
Issel Parra <14161286+isselparra@users.noreply.github.com>
2024-01-20 23:37:32 +0100

    refactor: remove redundant returns from switch in category factory provider

--- java/src/main/java/yatzykata/yatzy/domain/category/CategoryFactoryProvider.java
+++ java/src/main/java/yatzykata/yatzy/domain/category/CategoryFactoryProvider.java
@@ -6,55 +6,22 @@ import yatzykata.yatzy.domain.die.model.DieSide;
 
 public class CategoryFactoryProvider {
   public CategoryFactory getCategoryFactory(CategoryType categoryType) {
-    switch (categoryType) {
-      case CHANCE -> {
-        return new ChanceFactory();
-      }
-      case YATZY -> {
-        return new YatzyFactory();
-      }
-      case ONES -> {
-        return new RepeatedDieFactory(DieSide.ONE);
-      }
-      case TWOS -> {
-        return new RepeatedDieFactory(DieSide.TWO);
-      }
-      case THREES -> {
-        return new RepeatedDieFactory(DieSide.THREE);
-      }
-      case FOURS -> {
-        return new RepeatedDieFactory(DieSide.FOUR);
-      }
-      case FIVES -> {
-        return new RepeatedDieFactory(DieSide.FIVE);
-      }
-      case SIXES -> {
-        return new RepeatedDieFactory(DieSide.SIX);
-      }
-      case SMALL_STRAIGHT -> {
-        return new StraightFactory(StraightType.SMALL);
-      }
-      case LARGE_STRAIGHT -> {
-        return new StraightFactory(StraightType.LARGE);
-      }
-      case PAIR -> {
-        return new PairFactory();
-      }
-      case TWO_PAIRS -> {
-        return new TwoPairsFactory();
-      }
-      case THREE_OF_A_KIND -> {
-        return new ThreeOfAKindFactory();
-      }
-      case FOUR_OF_A_KIND -> {
-        return new FourOfAKindFactory();
-      }
-      case FULL_HOUSE -> {
-        return new FullHouseFactory();
-      }
-      default -> {
-        throw new IllegalArgumentException("Invalid category type: " + categoryType);
-      }
-    }
+    return switch (categoryType) {
+      case CHANCE -> new ChanceFactory();
+      case YATZY -> new YatzyFactory();
+      case ONES -> new RepeatedDieFactory(DieSide.ONE);
+      case TWOS -> new RepeatedDieFactory(DieSide.TWO);
+      case THREES -> new RepeatedDieFactory(DieSide.THREE);
+      case FOURS -> new RepeatedDieFactory(DieSide.FOUR);
+      case FIVES -> new RepeatedDieFactory(DieSide.FIVE);
+      case SIXES -> new RepeatedDieFactory(DieSide.SIX);
+      case SMALL_STRAIGHT -> new StraightFactory(StraightType.SMALL);
+      case LARGE_STRAIGHT -> new StraightFactory(StraightType.LARGE);
+      case PAIR -> new PairFactory();
+      case TWO_PAIRS -> new TwoPairsFactory();
+      case THREE_OF_A_KIND -> new ThreeOfAKindFactory();
+      case FOUR_OF_A_KIND -> new FourOfAKindFactory();
+      case FULL_HOUSE -> new FullHouseFactory();
+    };
   }
 }
