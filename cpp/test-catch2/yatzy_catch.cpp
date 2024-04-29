1e0dfdbedb858284edfe78e628ad9598949a5205
emily <emily@bacheconsulting.com>
2024-04-29 10:22:29 +0200

    improve whitespace

--- cpp/test-catch2/yatzy_catch.cpp
+++ cpp/test-catch2/yatzy_catch.cpp
@@ -15,64 +15,92 @@ static int *ints(int a, int b, int c, int d, int e) {
 }
 
 TEST_CASE ("Yatzy") {
-    SECTION("Chance scores sum of all dice") {
+    SECTION("Chance scores sum of all dice")
+    {
         int expected = 15;
         int actual = Yatzy1().Chance(2, 3, 4, 5, 1);
         CHECK(expected == actual);
         CHECK(16 == Yatzy1().Chance(3, 3, 4, 5, 1));
-    }SECTION("Yatzy scores 50") {
+    }
+    SECTION("Yatzy scores 50") {
         int expected = 50;
         int actual = Yatzy1().yatzy(ints(4, 4, 4, 4, 4));
         CHECK(expected == actual);
         CHECK(50 == Yatzy1().yatzy(ints(6, 6, 6, 6, 6)));
         CHECK(0 == Yatzy1().yatzy(ints(6, 6, 6, 6, 3)));
-    }SECTION("Test 1s") {
+    }
+    SECTION("Test 1s")
+    {
         CHECK(Yatzy1().Ones(1, 2, 3, 4, 5) == 1);
         CHECK(2 == Yatzy1().Ones(1, 2, 1, 4, 5));
         CHECK(0 == Yatzy1().Ones(6, 2, 2, 4, 5));
         CHECK(4 == Yatzy1().Ones(1, 2, 1, 1, 1));
-    }SECTION("test 2s") {
+    }
+    SECTION("test 2s")
+    {
         CHECK(4 == Yatzy1().Twos(1, 2, 3, 2, 6));
         CHECK(10 == Yatzy1().Twos(2, 2, 2, 2, 2));
-    }SECTION("test threes") {
+    }
+    SECTION("test threes")
+    {
         CHECK(6 == Yatzy1().Threes(1, 2, 3, 2, 3));
         CHECK(12 == Yatzy1().Threes(2, 3, 3, 3, 3));
-    }SECTION("fours test") {
+    }
+    SECTION("fours test")
+    {
         CHECK(12 == (new Yatzy1(4, 4, 4, 5, 5))->Fours());
         CHECK(8 == (new Yatzy1(4, 4, 5, 5, 5))->Fours());
         CHECK(4 == (*new Yatzy1(4, 5, 5, 5, 5)).Fours());
-    } SECTION("fives") {
+    }
+    SECTION("fives")
+    {
         CHECK(10 == (new Yatzy1(4, 4, 4, 5, 5))->Fives());
         CHECK(15 == Yatzy1(4, 4, 5, 5, 5).Fives());
         CHECK(20 == Yatzy1(4, 5, 5, 5, 5).Fives());
-    }SECTION("sixes test") {
+    }
+    SECTION("sixes test")
+    {
         CHECK(0 == Yatzy1(4, 4, 4, 5, 5).sixes());
         CHECK(6 == Yatzy1(4, 4, 6, 5, 5).sixes());
         CHECK(18 == Yatzy1(6, 5, 6, 6, 5).sixes());
-    }SECTION("one pair") {
+    }
+    SECTION("one pair")
+    {
         CHECK(6 == Yatzy1().ScorePair(3, 4, 3, 5, 6));
         CHECK(10 == Yatzy1().ScorePair(5, 3, 3, 3, 5));
         CHECK(12 == Yatzy1().ScorePair(5, 3, 6, 6, 5));
-    } SECTION("two Pair") {
+    }
+    SECTION("two Pair")
+    {
         CHECK(16 == Yatzy1().TwoPair(3, 3, 5, 4, 5));
         CHECK(16 == Yatzy1().TwoPair(3, 3, 5, 5, 5));
-    }SECTION("three of a kind") {
+    }
+    SECTION("three of a kind")
+    {
         CHECK(9 == Yatzy1().ThreeOfAKind(3, 3, 3, 4, 5));
         CHECK(15 == Yatzy1().ThreeOfAKind(5, 3, 5, 4, 5));
         CHECK(9 == Yatzy1::ThreeOfAKind(3, 3, 3, 3, 5));
-    }SECTION("four of a kind") {
+    }
+    SECTION("four of a kind")
+    {
         CHECK(12 == Yatzy1::FourOfAKind(3, 3, 3, 3, 5));
         CHECK(20 == Yatzy1::FourOfAKind(5, 5, 5, 4, 5));
         CHECK(12 == Yatzy1::FourOfAKind(3, 3, 3, 3, 3));
-    } SECTION("smallStraight") {
+    }
+    SECTION("smallStraight")
+    {
         CHECK(15 == Yatzy1::SmallStraight(1, 2, 3, 4, 5));
         CHECK(15 == Yatzy1::SmallStraight(2, 3, 4, 5, 1));
         CHECK(0 == Yatzy1().SmallStraight(1, 2, 2, 4, 5));
-    }SECTION("largeStraight") {
+    }
+    SECTION("largeStraight")
+    {
         CHECK(20 == Yatzy1::LargeStraight(6, 2, 3, 4, 5));
         CHECK(20 == Yatzy1().LargeStraight(2, 3, 4, 5, 6));
         CHECK(0 == Yatzy1::LargeStraight(1, 2, 2, 4, 5));
-    } SECTION("fullHouse") {
+    }
+    SECTION("fullHouse")
+    {
         CHECK(18 == Yatzy1().FullHouse(6, 2, 2, 2, 6));
         CHECK(0 == Yatzy1().FullHouse(2, 3, 4, 5, 6));
     }
