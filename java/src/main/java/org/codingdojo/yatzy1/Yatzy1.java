4f5725951547541487dad51a6eaa5607fc8a7276
ET34599 <rihab.boudhriwa@bred.fr>
2024-05-09 20:06:06 +0200

    implement and refacto Yatzy1

--- java/src/main/java/org/codingdojo/yatzy1/Yatzy1.java
+++ java/src/main/java/org/codingdojo/yatzy1/Yatzy1.java
@@ -13,79 +13,93 @@ public class Yatzy1 {
      * calculate the sum of all the dices
      * @return the sum of all the dices
      */
-    public  int chance()
-    {
-        int total = 0;
-        return total;
+    public int getChanceScore() {
+        return dices.stream().reduce(0, Integer::sum);
     }
 
     /**
      * @return 50 if all the dices are equals otherwise 0
      */
-    public  int yatzyScore()
-    {
-        return 0;
+    public int yatzyScore() {
+        return (dices.stream().distinct().count() == 1 ? YATZY_SCORE : 0);
     }
 
     /**
      * calculate the  sum of all the dices with the value sideNumber
-     * @param the number of the side we are looking for
+     * @sideNumber the number of the side we are looking for
      * @return the  sum of all the dices with the value sideNumber
      */
     public int getScoreBySideFrequency(int sideNumber) {
-        return 0;
+        return YatzyHelper.findFrequency(sideNumber, dices) * sideNumber;
     }
-
     /**
      *  calculate the score the pairs
      * @return the sum the all pairs * 2
      */
-    public int getScoreOfPair()
-    {
-        return 0;
+    public int getScoreOfpair() {
+        var numberPresentInPairs = YatzyHelper.getPairs(dices);
+        if (numberPresentInPairs != null) {
+            var pairValue = numberPresentInPairs.stream().sorted(Comparator.reverseOrder()).findFirst();
+            if (pairValue.isPresent()) {
+                return (pairValue.get()) * 2;
+            }
+            return 0;
+        } else
+            return 0;
     }
     /**
      *  calculate the score if there are two pairs
      * @return the sum the two pairs * 2
      */
-    public static int getScoreOfTowPair()
-    {
-        return 0;
+    public int getScoreOfTwoPair() {
+        var numberPresentInPairs = YatzyHelper.getPairs(dices);
+        if (numberPresentInPairs.size() == 2) {
+            return (numberPresentInPairs.stream()
+                .mapToInt(Integer::intValue)
+                .sum()) * 2;
+        } else
+            return 0;
     }
-
     /**
      * calculate score of the repeated dices n time
-     * @param frenquencyNumber how many the dice is repeated
+     * @param frequencyNumber how many the dice is repeated
      * @return sum of the repeated dices
      */
-    public static int getScoreOfRepeatedSideByFrequencyNumber(int frenquencyNumber)
+    public  int getScoreOfRepeatedSideByFrequencyNumber(int frequencyNumber)
     {
-        return 0;
+        return YatzyHelper.getRepeatedSideByFrequencyNumber(frequencyNumber, dices) * frequencyNumber;
     }
 
     /**
      *  the score if dices  contains 1,2,3,4,5
      * @return SMALL_STRAIGHT_SCORE if we have small straight otherwise 0
      */
-    public static int getLargeStraightScore()
-    {
+    public int getSmallStraightScore() {
+        if (YatzyHelper.isStraight(dices) && YatzyHelper.findFrequency(6, dices) == 0) {
+            return SMALL_STRAIGHT_SCORE;
+        }
         return 0;
     }
     /**
      *  the score if dices  contains 2,3,4,5,6
      * @return LARGE_STRAIGHT_SCORE if we have large straight otherwise 0
      */
-    public static int getLargeStraightScore()
-    {
+    public int getLargeStraightScore() {
+        if (YatzyHelper.isStraight(dices) && YatzyHelper.findFrequency(1, dices) == 0) {
+            return LARGE_STRAIGHT_SCORE;
+        }
         return 0;
     }
-
     /**
      *  calculate the score if there is a pair and three repeated dices
      * @return the score of a pair and three repeated dices
      */
-    public static int getFullHouseScore()
-    {
+    public int getFullHouseScore() {
+        int repeatedTwice = YatzyHelper.getRepeatedSideByFrequencyNumber(2, dices);
+        int repeatedThreeTimes = YatzyHelper.getRepeatedSideByFrequencyNumber(3, dices);
+        if (repeatedTwice != 0 && repeatedThreeTimes != 0) {
+            return repeatedThreeTimes * 3 + repeatedTwice * 2;
+        }
         return 0;
     }
 }
--- java/src/main/java/org/codingdojo/yatzy1/YatzyHelper.java
+++ java/src/main/java/org/codingdojo/yatzy1/YatzyHelper.java
@@ -10,20 +10,55 @@ import java.util.stream.IntStream;
 
 @UtilityClass
 public class YatzyHelper {
-
+    /**
+     *
+     * @param number whose  frequency to detemined
+     * @param diceValues
+     * @return the frequency of th number in the diceValues
+     */
     public static int findFrequency(int number, List<Integer> diceValues) {
-        return 0;
+        return diceValues != null ? Collections.frequency(diceValues, number) : 0;
     }
 
+    /**
+     *
+     * @param frequencyNumber
+     * @param diceValues
+     * @return the value whose repeated n times (n = frequencyNumber)
+     */
     public static int getRepeatedSideByFrequencyNumber(int frequencyNumber, List<Integer> diceValues) {
-        return 0;
+        return diceValues.stream()
+            .sorted(Comparator.reverseOrder())
+            .filter(side -> findFrequency(side, diceValues) >= frequencyNumber)
+            .findFirst()
+            .orElse(0);
     }
 
+    /**
+     *
+     * @param diceValues
+     * @return if the list is contains straight or not
+     */
     public static boolean isStraight(List<Integer> diceValues) {
-        return 0;
+        int[] s = diceValues.stream().sorted().mapToInt(Integer::intValue).toArray();
+        IntStream differences =
+            IntStream.range(0, s.length - 1)
+                .map(i -> s[i + 1] - s[i]).filter(f -> f != 1);
+        return differences.count() == 0;
     }
 
-    public static List<Integer> getPairs(List<Integer> diceValues) {
-        return 0;
+    /**
+     *
+     * @param diceValues
+     * @return the pairs of dices
+     */
+    public static Set<Integer> getPairs(List<Integer> diceValues) {
+        Set<Integer> allPossiblePairs = new HashSet<>();
+        diceValues.forEach(value -> {
+            if (findFrequency(value.intValue(), diceValues) >= 2) {
+                allPossiblePairs.add(value);
+            }
+        });
+        return allPossiblePairs;
     }
 }
