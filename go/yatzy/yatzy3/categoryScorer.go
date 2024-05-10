56ea3f43db2ae7f684e7619bc53a259ee09e2a65
Ian Wodder <ian.wodder@gmail.com>
2024-05-09 19:08:23 -0600

    add yatzyScorer

--- go/yatzy/yatzy3/categoryScorer.go
+++ go/yatzy/yatzy3/categoryScorer.go
@@ -1,9 +1,11 @@
 package yatzy3
 
-import "github.com/emilybache/yatzy-refactoring-kata/yatzy"
+import (
+	"github.com/emilybache/yatzy-refactoring-kata/yatzy"
+)
 
 type categoryScorer interface {
-	calculateScore([]int) int
+	calculateScore(dice []int) int
 }
 
 func newCategoryScorer(categoryName string) categoryScorer {
@@ -11,6 +13,8 @@ func newCategoryScorer(categoryName string) categoryScorer {
 	switch category {
 	case yatzy.Categories.CHANCE:
 		return chanceScorer{}
+	case yatzy.Categories.YATZY:
+		return yatzyScorer{}
 	default:
 		return nilScorer{}
 	}
@@ -22,14 +26,36 @@ func (chanceScorer) calculateScore(dice []int) int {
 	return sum(dice)
 }
 
+type yatzyScorer struct{}
+
+func (yatzyScorer) calculateScore(dice []int) int {
+	for _, v := range frequencies(dice) {
+		if v == 5 {
+			return 50
+		}
+	}
+	return 0
+}
+
 type nilScorer struct{}
 
 func (nilScorer) calculateScore([]int) int { return 0 }
 
-func sum(dice []int) int {
+func sum(d []int) int {
 	var s int
-	for _, die := range dice {
-		s += die
+	for _, d2 := range d {
+		s += d2
 	}
 	return s
+}
+
+func frequencies(dice []int) map[int]int {
+	diceFrequencies := map[int]int{}
+	for _, i := range dice {
+		diceFrequencies[i] = 0
+	}
+	for i := range dice {
+		diceFrequencies[dice[i]] += 1
+	}
+	return diceFrequencies
 }
