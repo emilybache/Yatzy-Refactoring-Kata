ddda76a87c1f0e5c3276aaf35b55cefcb4ecd50c
Nupur Shah <noopur1010@outlook.com>
2024-05-25 09:37:46 +1000

    refector code to Yatzy2 and Update Yatzy.Test.csproj with new package references

--- csharp/Yatzy2/Yatzy2.cs
+++ csharp/Yatzy2/Yatzy2.cs
@@ -2,224 +2,110 @@
 
 public class Yatzy2
 {
-    static readonly List<int> DICE_VALUES = new List<int> { 6, 5, 4, 3, 2, 1 };
+    /* Notes 
+     * Removed unwated code
+     * Early exits if not get categoryName or empty dice 
+     * DICE_VALUES converted into IReadOnlyList - need to access the elements   
+     * Static methods for each calculation to improve performance
+     * ALl methods are private - no need to expose them
+     * Creates InitializeDiceFrequencies method to initialize the dictionary - one method to create the dictionary     * 
+     * reduce duplication of code
+     * removed unnecessary foreach loop and replaed with Lamda expression      * 
+     */
 
-    public List<string> ValidCategories()
+    private static readonly IReadOnlyList<int> DICE_VALUES = new List<int> { 6, 5, 4, 3, 2, 1 };
+
+    private static Dictionary<int, int> InitializeDiceFrequencies(IEnumerable<int> dice)
     {
-        return Enum.GetNames(typeof(YatzyCategory)).ToList();
+        var diceFrequencies = DICE_VALUES.ToDictionary(i => i, i => 0);
+
+        var updateDict = dice
+            .GroupBy(die => die)
+            .ToDictionary(group => group.Key, group => group.Count());
+
+        updateDict.Keys
+            .ToList()
+            .ForEach(key => diceFrequencies[key] = updateDict[key]);
+
+        return diceFrequencies;
     }
 
-    public int Score(List<int> dice, string categoryName)
-    {
-        YatzyCategory category = (YatzyCategory)Enum.Parse(typeof(YatzyCategory), categoryName);
+    private static int MultiplyDiceFrequencies(Dictionary<int, int> frequenciesDice, int value) =>
+        frequenciesDice.TryGetValue(value, out int count) ? count * value : 0;
 
-        // calculate dice frequencies
-        Dictionary<int, int> diceFrequencies = new Dictionary<int, int>();
-        foreach (int i in DICE_VALUES)
+    private static int MultiplySameDiceFrequencies(Dictionary<int, int> frequenciesDice, int sameDiceNumber) =>
+        DICE_VALUES
+        .Where(i => frequenciesDice[i] >= sameDiceNumber)
+        .Select(i => i * sameDiceNumber)
+        .FirstOrDefault();
+    private static int CountDiceFrequencies(Dictionary<int, int> diceFrequencies,
+        Func<int, bool> predicate) =>
+        diceFrequencies.Values.Count(predicate);
+
+    private static bool CompareCountDiceFrequencies(int compareCountNo,
+        Dictionary<int, int> diceFrequencies,
+        Func<int, bool> predicate) =>
+        CountDiceFrequencies(diceFrequencies, predicate) == compareCountNo;
+
+    private static int CalculateStraightValue(int compareCountNo,
+        int diceFrequenciesNo,
+        IEnumerable<int> dice,
+        Dictionary<int, int> diceFrequencies,
+        Func<int, bool> predicate) =>
+        (CompareCountDiceFrequencies(compareCountNo, diceFrequencies, predicate) && diceFrequencies[diceFrequenciesNo] == 0)
+        ? dice.Sum()
+        : 0;
+
+    private static int CalculateTwoPairsValue(int compareCountNo,
+        Dictionary<int, int> diceFrequencies,
+        Func<int, bool> predicate) =>
+        CompareCountDiceFrequencies(compareCountNo, diceFrequencies, predicate)
+        ? DICE_VALUES.Where(i => diceFrequencies[i] >= 2).Sum(i => i * 2)
+        : 0;
+
+    private static int GetFullHouseValue(Dictionary<int, int> diceFrequencies, IEnumerable<int> dice) =>
+        (diceFrequencies.ContainsValue(2) && diceFrequencies.ContainsValue(3)) ? dice.Sum() : 0;
+
+    private static int GetScoreByCategory(YatzyCategory category, Dictionary<int, int> diceFrequencies, IEnumerable<int> dice) =>
+        category switch
         {
-            diceFrequencies[i] = 0;
-        }
-        foreach (int die in dice)
-        {
-            diceFrequencies[die]++;
-        }
-        
-        // calculate the score
-        int result;
-        switch (category)
-        {
-            case YatzyCategory.CHANCE:
+            YatzyCategory.CHANCE => dice.Sum(),
+            YatzyCategory.YATZY => diceFrequencies.ContainsValue(5) ? 50 : 0,
+            YatzyCategory.ONES => MultiplyDiceFrequencies(diceFrequencies, 1),
+            YatzyCategory.TWOS => MultiplyDiceFrequencies(diceFrequencies, 2),
+            YatzyCategory.THREES => MultiplyDiceFrequencies(diceFrequencies, 3),
+            YatzyCategory.FOURS => MultiplyDiceFrequencies(diceFrequencies, 4),
+            YatzyCategory.FIVES => MultiplyDiceFrequencies(diceFrequencies, 5),
+            YatzyCategory.SIXES => MultiplyDiceFrequencies(diceFrequencies, 6),
+            YatzyCategory.PAIR => MultiplySameDiceFrequencies(diceFrequencies, 2),
+            YatzyCategory.THREE_OF_A_KIND => MultiplySameDiceFrequencies(diceFrequencies, 3),
+            YatzyCategory.FOUR_OF_A_KIND => MultiplySameDiceFrequencies(diceFrequencies, 4),
+            YatzyCategory.SMALL_STRAIGHT => CalculateStraightValue(5, 6, dice, diceFrequencies, frequency => frequency == 1),
+            YatzyCategory.LARGE_STRAIGHT => CalculateStraightValue(5, 1, dice, diceFrequencies, frequency => frequency == 1),
+            YatzyCategory.TWO_PAIRS => CalculateTwoPairsValue(2, diceFrequencies, frequency => frequency >= 2),
+            YatzyCategory.FULL_HOUSE => GetFullHouseValue(diceFrequencies, dice),
+            _ => 0  // Default case if none of the above matches
+        };
 
-                // chance sums the dice
-                result = dice.Sum();
-                break;
 
-            case YatzyCategory.YATZY:
+    /* early exits if not get categoryName */
+    private static bool ScoreInputDiceValidation(IEnumerable<int> dice) =>
+       dice == null || !dice.Any()
+       ? throw new ArgumentException("Dice cannot be null or empty", nameof(dice))
+       : true;
 
-                // score for yatzy if all dice are the same
-                int yatzyResult = 0;
-                if (diceFrequencies.ContainsValue(5))
-                {
-                    yatzyResult = 50;
-                }
+    /* early exits if not get categoryName */
+    private static YatzyCategory ScoreInputCategoryValidation(string categoryName, IEnumerable<int> dice) =>
+        ScoreInputDiceValidation(dice) && Enum.TryParse(categoryName, out YatzyCategory category)
+        ? category
+        : throw new ArgumentException("Invalid category name", nameof(categoryName));
 
-                result = yatzyResult;
-                break;
 
-            case YatzyCategory.ONES:
-                // sum all the ones
-                result = diceFrequencies[1];
-                break;
-
-            case YatzyCategory.TWOS:
-                // sum all the twos
-                result = diceFrequencies[2] * 2;
-                break;
-
-            case YatzyCategory.THREES:
-                // sum all the threes
-                result = diceFrequencies[3] * 3;
-                break;
-
-            case YatzyCategory.FOURS:
-                // sum all the fours
-                result = diceFrequencies[4] * 4;
-                break;
-
-            case YatzyCategory.FIVES:
-                // sum all the fives
-                result = diceFrequencies[5] * 5;
-                break;
-
-            case YatzyCategory.SIXES:
-                // sum all the sixes
-                result = diceFrequencies[6] * 6;
-                break;
-
-            case YatzyCategory.PAIR:
-
-                // score pair if two dice are the same
-                int pairResult = 0;
-                // score highest pair if there is more than one
-                foreach (int i in DICE_VALUES)
-                {
-                    if (diceFrequencies[i] >= 2)
-                    {
-                        pairResult = i * 2;
-                        break;
-                    }
-                }
-
-                result = pairResult;
-                break;
-
-            case YatzyCategory.THREE_OF_A_KIND:
-
-                // score if three dice are the same
-                int threeKindResult = 0;
-                foreach (int i in DICE_VALUES)
-                {
-                    if (diceFrequencies[i] >= 3)
-                    {
-                        threeKindResult = i * 3;
-                        break;
-                    }
-                }
-
-                result = threeKindResult;
-                break;
-
-            case YatzyCategory.FOUR_OF_A_KIND:
-
-                // score if four dice are the same
-                int fourKindResult = 0;
-                foreach (int i in DICE_VALUES)
-                {
-                    if (diceFrequencies[i] >= 4)
-                    {
-                        fourKindResult = i * 4;
-                        break;
-                    }
-                }
-
-                result = fourKindResult;
-                break;
-
-            case YatzyCategory.SMALL_STRAIGHT:
-
-                // score if dice contains 1,2,3,4,5
-                int smallStraightResult = 0;
-                long count = 0L;
-                foreach (int frequency in diceFrequencies.Values)
-                {
-                    if (frequency == 1)
-                    {
-                        count++;
-                    }
-                }
-
-                if (count == 5 && diceFrequencies[6] == 0)
-                {
-                    foreach (int die in dice)
-                    {
-                        smallStraightResult += die;
-                    }
-                }
-
-                result = smallStraightResult;
-                break;
-
-            case YatzyCategory.LARGE_STRAIGHT:
-
-                // score if dice contains 2,3,4,5,6
-                int largeStraightResult = 0;
-                long straightCount = 0L;
-                foreach (int frequency in diceFrequencies.Values)
-                {
-                    if (frequency == 1)
-                    {
-                        straightCount++;
-                    }
-                }
-
-                if (straightCount == 5 && diceFrequencies[1] == 0)
-                {
-                    foreach (int die in dice)
-                    {
-                        largeStraightResult += die;
-                    }
-                }
-
-                result = largeStraightResult;
-                break;
-
-            case YatzyCategory.TWO_PAIRS:
-
-                // score if there are two pairs
-                int twoPairResult = 0;
-                long pairCount = 0L;
-                foreach (int frequency in diceFrequencies.Values)
-                {
-                    if (frequency >= 2)
-                    {
-                        pairCount++;
-                    }
-                }
-
-                if (pairCount == 2)
-                {
-                    foreach (int i in DICE_VALUES)
-                    {
-                        if (diceFrequencies[i] >= 2)
-                        {
-                            twoPairResult += i * 2;
-                        }
-                    }
-                }
-
-                result = twoPairResult;
-                break;
-
-            case YatzyCategory.FULL_HOUSE:
-
-                // score if there is a pair as well as three of a kind
-                int fullHouseResult = 0;
-                if (diceFrequencies.ContainsValue(2) && diceFrequencies.ContainsValue(3))
-                {
-                    foreach (int die in dice)
-                    {
-                        fullHouseResult += die;
-                    }
-                }
-
-                result = fullHouseResult;
-                break;
-
-            default:
-                result = 0;
-                break;
-        }
-
-        return result;
-    }
+    /// <summary>
+    /// Calculates the score for a given set of dice and category.
+    /// </summary>
+    /// <param name="dice">The values of the dice.</param>
+    /// <param name="categoryName">The category for scoring.</param>
+    /// <returns>The score for the given dice and category.</returns>
+    public int Score(IEnumerable<int> dice, string categoryName) => GetScoreByCategory(ScoreInputCategoryValidation(categoryName, dice), InitializeDiceFrequencies(dice), dice);
 }
