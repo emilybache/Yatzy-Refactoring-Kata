1932449c43f3ec729e8d6392fa16b0d7a3d14dab
Matt Clarke <matto.kuraku@gmail.com>
2025-05-11 10:39:49 +0200

    Remove outdated reference to py.test

--- python/test_yatzy1.py
+++ python/test_yatzy1.py
@@ -1,9 +1,6 @@
 from yatzy1 import Yatzy
 
 
-# These unit tests can be run using the py.test framework
-# available from http://pytest.org/
-
 def test_chance_scores_sum_of_all_dice():
     expected = 15
     actual = Yatzy.chance(2, 3, 4, 5, 1)
