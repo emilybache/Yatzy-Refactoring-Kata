c29e421a5c1d23f8f083557c2087c18bbd747905
Aurélien Mino <aurelien.mino@gmail.com>
2025-11-08 20:22:39 +0100

    chore(java): remove unnecessary public accessors and unused field

--- java/src/test/java/org/codingdojo/YatzyOnePlusTest.java
+++ java/src/test/java/org/codingdojo/YatzyOnePlusTest.java
@@ -2,7 +2,6 @@ package org.codingdojo;
 
 import org.codingdojo.yatzy2.Yatzy2;
 import org.codingdojo.yatzy3.Yatzy3;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -12,8 +11,7 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-public class YatzyOnePlusTest {
-    Yatzy2 yatzy2 = new Yatzy2();
+class YatzyOnePlusTest {
 
     static Stream<YatzyCalculator> yatzyProvider() {
         return Stream.of(new Yatzy2(), new Yatzy3());
@@ -21,14 +19,14 @@ public class YatzyOnePlusTest {
 
     @ParameterizedTest
     @MethodSource("yatzyProvider")
-    public void chance_scores_sum_of_all_dice(YatzyCalculator calculator) {
+    void chance_scores_sum_of_all_dice(YatzyCalculator calculator) {
         assertEquals(15, calculator.score(List.of(2,3,4,5,1), "CHANCE"));
         assertEquals(16, calculator.score(List.of(3,3,4,5,1), "CHANCE"));
     }
 
     @ParameterizedTest
     @MethodSource("yatzyProvider")
-    public void yatzy_scores_50(YatzyCalculator yatzy2) {
+    void yatzy_scores_50(YatzyCalculator yatzy2) {
         assertEquals(50, yatzy2.score(List.of(4,4,4,4,4), "YATZY"));
         assertEquals(50, yatzy2.score(List.of(6,6,6,6,6), "YATZY"));
         assertEquals(0, yatzy2.score(List.of(6,6,6,6,3), "YATZY"));
@@ -36,7 +34,7 @@ public class YatzyOnePlusTest {
 
     @ParameterizedTest
     @MethodSource("yatzyProvider")
-    public void test_1s(YatzyCalculator yatzy2) {
+    void test_1s(YatzyCalculator yatzy2) {
         assertEquals(1, yatzy2.score(List.of(1,2,3,4,5), "ONES"));
         assertEquals(2, yatzy2.score(List.of(1,2,1,4,5), "ONES"));
         assertEquals(0, yatzy2.score(List.of(6,2,2,4,5), "ONES"));
@@ -46,7 +44,7 @@ public class YatzyOnePlusTest {
     @ParameterizedTest
     @MethodSource("yatzyProvider")
 
-    public void twos(YatzyCalculator yatzy2) {
+    void twos(YatzyCalculator yatzy2) {
         assertEquals(4, yatzy2.score(List.of(1,2,3,2,6), "TWOS"));
         assertEquals(10, yatzy2.score(List.of(2,2,2,2,2), "TWOS"));
     }
@@ -54,7 +52,7 @@ public class YatzyOnePlusTest {
     @ParameterizedTest
     @MethodSource("yatzyProvider")
 
-    public void threes(YatzyCalculator yatzy2) {
+    void threes(YatzyCalculator yatzy2) {
         assertEquals(6, yatzy2.score(List.of(1,2,3,2,3), "THREES"));
         assertEquals(12, yatzy2.score(List.of(2,3,3,3,3), "THREES"));
     }
@@ -62,7 +60,7 @@ public class YatzyOnePlusTest {
     @ParameterizedTest
     @MethodSource("yatzyProvider")
 
-    public void fours(YatzyCalculator yatzy2)
+    void fours(YatzyCalculator yatzy2)
     {
         assertEquals(12, yatzy2.score(List.of(4,4,4,5,5), "FOURS"));
         assertEquals(8, yatzy2.score(List.of(4,4,5,5,5), "FOURS"));
@@ -71,14 +69,14 @@ public class YatzyOnePlusTest {
 
     @ParameterizedTest
     @MethodSource("yatzyProvider")
-    public void fives(YatzyCalculator yatzy2) {
+    void fives(YatzyCalculator yatzy2) {
         assertEquals(10, yatzy2.score(List.of(4,4,4,5,5), "FIVES"));
         assertEquals(15, yatzy2.score(List.of(4,4,5,5,5), "FIVES"));
         assertEquals(20, yatzy2.score(List.of(4,5,5,5,5), "FIVES"));
     }
     @ParameterizedTest
     @MethodSource("yatzyProvider")
-    public void sixes(YatzyCalculator yatzy2) {
+    void sixes(YatzyCalculator yatzy2) {
         assertEquals(0, yatzy2.score(List.of(4,4,4,5,5), "SIXES"));
         assertEquals(6, yatzy2.score(List.of(4,4,6,5,5), "SIXES"));
         assertEquals(18, yatzy2.score(List.of(6,5,6,6,5), "SIXES"));
@@ -86,7 +84,7 @@ public class YatzyOnePlusTest {
 
     @ParameterizedTest
     @MethodSource("yatzyProvider")
-    public void pair(YatzyCalculator yatzy2) {
+    void pair(YatzyCalculator yatzy2) {
         assertEquals(6, yatzy2.score(List.of(3,4,3,5,6), "PAIR"));
         assertEquals(10, yatzy2.score(List.of(5,3,3,3,5), "PAIR"));
         assertEquals(12, yatzy2.score(List.of(5,3,6,6,5), "PAIR"));
@@ -94,14 +92,14 @@ public class YatzyOnePlusTest {
 
     @ParameterizedTest
     @MethodSource("yatzyProvider")
-    public void two_pair(YatzyCalculator yatzy2) {
+    void two_pair(YatzyCalculator yatzy2) {
         assertEquals(16, yatzy2.score(List.of(3,3,5,4,5), "TWO_PAIRS"));
         assertEquals(16, yatzy2.score(List.of(3,3,5,5,5), "TWO_PAIRS"));
     }
 
     @ParameterizedTest
     @MethodSource("yatzyProvider")
-    public void three_of_a_kind(YatzyCalculator yatzy2)
+    void three_of_a_kind(YatzyCalculator yatzy2)
     {
         assertEquals(9, yatzy2.score(List.of(3,3,3,4,5), "THREE_OF_A_KIND"));
         assertEquals(15, yatzy2.score(List.of(5,3,5,4,5), "THREE_OF_A_KIND"));
@@ -110,7 +108,7 @@ public class YatzyOnePlusTest {
 
     @ParameterizedTest
     @MethodSource("yatzyProvider")
-    public void four_of_a_knd(YatzyCalculator yatzy2) {
+    void four_of_a_knd(YatzyCalculator yatzy2) {
         assertEquals(12, yatzy2.score(List.of(3,3,3,3,5), "FOUR_OF_A_KIND"));
         assertEquals(20, yatzy2.score(List.of(5,5,5,4,5), "FOUR_OF_A_KIND"));
         assertEquals(12, yatzy2.score(List.of(3,3,3,3,3), "FOUR_OF_A_KIND"));
@@ -118,7 +116,7 @@ public class YatzyOnePlusTest {
 
     @ParameterizedTest
     @MethodSource("yatzyProvider")
-    public void smallStraight(YatzyCalculator yatzy2) {
+    void smallStraight(YatzyCalculator yatzy2) {
         assertEquals(15, yatzy2.score(List.of(1,2,3,4,5), "SMALL_STRAIGHT"));
         assertEquals(15, yatzy2.score(List.of(2,3,4,5,1), "SMALL_STRAIGHT"));
         assertEquals(0, yatzy2.score(List.of(1,2,2,4,5), "SMALL_STRAIGHT"));
@@ -126,14 +124,14 @@ public class YatzyOnePlusTest {
 
     @ParameterizedTest
     @MethodSource("yatzyProvider")
-    public void largeStraight(YatzyCalculator yatzy2) {
+    void largeStraight(YatzyCalculator yatzy2) {
         assertEquals(20, yatzy2.score(List.of(6,2,3,4,5), "LARGE_STRAIGHT"));
         assertEquals(20, yatzy2.score(List.of(2,3,4,5,6), "LARGE_STRAIGHT"));
         assertEquals(0, yatzy2.score(List.of(1,2,2,4,5), "LARGE_STRAIGHT"));
     }
     @ParameterizedTest
     @MethodSource("yatzyProvider")
-    public void fullHouse(YatzyCalculator yatzy2) {
+    void fullHouse(YatzyCalculator yatzy2) {
         assertEquals(18, yatzy2.score(List.of(6,2,2,2,6), "FULL_HOUSE"));
         assertEquals(0, yatzy2.score(List.of(2,3,4,5,6), "FULL_HOUSE"));
     }
