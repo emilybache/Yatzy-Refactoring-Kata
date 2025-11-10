6ff187be5cccbcb98b5f69e09dedf5631fc12ae2
Aurélien Mino <aurelien.mino@gmail.com>
2025-11-10 22:39:49 +0100

    Use approvals on java version

--- java/src/test/java/org/codingdojo/YatzyOnePlusTest.java
+++ java/src/test/java/org/codingdojo/YatzyOnePlusTest.java
@@ -1,139 +1,130 @@
 package org.codingdojo;
 
+import org.approvaltests.Approvals;
+import org.approvaltests.core.Options;
 import org.codingdojo.yatzy2.Yatzy2;
 import org.codingdojo.yatzy3.Yatzy3;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.FieldSource;
+import org.lambda.functions.Function5;
 
 import java.util.List;
-import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.approvaltests.combinations.CombinationApprovals.verifyAllCombinations;
+import static org.junit.jupiter.api.Named.named;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 
 class YatzyOnePlusTest {
 
-    static Stream<YatzyCalculator> yatzyProvider() {
-        return Stream.of(new Yatzy2(), new Yatzy3());
+    private static final Integer[] DICE_VALUES = {1, 2, 3, 4, 5, 6};
+
+    private static List<Arguments> yatzyCalculators = List.of(
+        arguments(named(Yatzy2.class.getSimpleName(), new Yatzy2())),
+        arguments(named(Yatzy3.class.getSimpleName(), new Yatzy3()))
+    );
+
+    {
+        Approvals.settings().allowMultipleVerifyCallsForThisClass();
     }
 
     @ParameterizedTest
-    @MethodSource("yatzyProvider")
+    @FieldSource("yatzyCalculators")
     void chance_scores_sum_of_all_dice(YatzyCalculator calculator) {
-        assertEquals(15, calculator.score(List.of(2,3,4,5,1), "CHANCE"));
-        assertEquals(16, calculator.score(List.of(3,3,4,5,1), "CHANCE"));
+        verifyAllCombinations(scoreOfCategory(calculator, "CHANCE"), DICE_VALUES, DICE_VALUES, DICE_VALUES, DICE_VALUES, DICE_VALUES);
     }
 
     @ParameterizedTest
-    @MethodSource("yatzyProvider")
-    void yatzy_scores_50(YatzyCalculator yatzy2) {
-        assertEquals(50, yatzy2.score(List.of(4,4,4,4,4), "YATZY"));
-        assertEquals(50, yatzy2.score(List.of(6,6,6,6,6), "YATZY"));
-        assertEquals(0, yatzy2.score(List.of(6,6,6,6,3), "YATZY"));
+    @FieldSource("yatzyCalculators")
+    void yatzy_scores_50(YatzyCalculator calculator) {
+        verifyAllCombinations(scoreOfCategory(calculator, "YATZY"), DICE_VALUES, DICE_VALUES, DICE_VALUES, DICE_VALUES, DICE_VALUES);
     }
 
     @ParameterizedTest
-    @MethodSource("yatzyProvider")
-    void test_1s(YatzyCalculator yatzy2) {
-        assertEquals(1, yatzy2.score(List.of(1,2,3,4,5), "ONES"));
-        assertEquals(2, yatzy2.score(List.of(1,2,1,4,5), "ONES"));
-        assertEquals(0, yatzy2.score(List.of(6,2,2,4,5), "ONES"));
-        assertEquals(4, yatzy2.score(List.of(1,2,1,1,1), "ONES"));
+    @FieldSource("yatzyCalculators")
+    void test_1s(YatzyCalculator calculator) {
+        verifyAllCombinations(scoreOfCategory(calculator, "ONES"), DICE_VALUES, DICE_VALUES, DICE_VALUES, DICE_VALUES, DICE_VALUES);
     }
 
     @ParameterizedTest
-    @MethodSource("yatzyProvider")
-
-    void twos(YatzyCalculator yatzy2) {
-        assertEquals(4, yatzy2.score(List.of(1,2,3,2,6), "TWOS"));
-        assertEquals(10, yatzy2.score(List.of(2,2,2,2,2), "TWOS"));
+    @FieldSource("yatzyCalculators")
+    void twos(YatzyCalculator calculator) {
+        verifyAllCombinations(scoreOfCategory(calculator, "TWOS"), DICE_VALUES, DICE_VALUES, DICE_VALUES, DICE_VALUES, DICE_VALUES);
     }
 
     @ParameterizedTest
-    @MethodSource("yatzyProvider")
-
-    void threes(YatzyCalculator yatzy2) {
-        assertEquals(6, yatzy2.score(List.of(1,2,3,2,3), "THREES"));
-        assertEquals(12, yatzy2.score(List.of(2,3,3,3,3), "THREES"));
+    @FieldSource("yatzyCalculators")
+    void threes(YatzyCalculator calculator) {
+        verifyAllCombinations(scoreOfCategory(calculator, "THREES"), DICE_VALUES, DICE_VALUES, DICE_VALUES, DICE_VALUES, DICE_VALUES);
     }
 
     @ParameterizedTest
-    @MethodSource("yatzyProvider")
-
-    void fours(YatzyCalculator yatzy2)
-    {
-        assertEquals(12, yatzy2.score(List.of(4,4,4,5,5), "FOURS"));
-        assertEquals(8, yatzy2.score(List.of(4,4,5,5,5), "FOURS"));
-        assertEquals(4, yatzy2.score(List.of(4,5,5,5,5), "FOURS"));
+    @FieldSource("yatzyCalculators")
+    void fours(YatzyCalculator calculator) {
+        verifyAllCombinations(scoreOfCategory(calculator, "FOURS"), DICE_VALUES, DICE_VALUES, DICE_VALUES, DICE_VALUES, DICE_VALUES);
     }
 
     @ParameterizedTest
-    @MethodSource("yatzyProvider")
-    void fives(YatzyCalculator yatzy2) {
-        assertEquals(10, yatzy2.score(List.of(4,4,4,5,5), "FIVES"));
-        assertEquals(15, yatzy2.score(List.of(4,4,5,5,5), "FIVES"));
-        assertEquals(20, yatzy2.score(List.of(4,5,5,5,5), "FIVES"));
+    @FieldSource("yatzyCalculators")
+    void fives(YatzyCalculator calculator) {
+        verifyAllCombinations(scoreOfCategory(calculator, "FIVES"), DICE_VALUES, DICE_VALUES, DICE_VALUES, DICE_VALUES, DICE_VALUES);
     }
     @ParameterizedTest
-    @MethodSource("yatzyProvider")
-    void sixes(YatzyCalculator yatzy2) {
-        assertEquals(0, yatzy2.score(List.of(4,4,4,5,5), "SIXES"));
-        assertEquals(6, yatzy2.score(List.of(4,4,6,5,5), "SIXES"));
-        assertEquals(18, yatzy2.score(List.of(6,5,6,6,5), "SIXES"));
+    @FieldSource("yatzyCalculators")
+    void sixes(YatzyCalculator calculator) {
+        verifyAllCombinations(scoreOfCategory(calculator, "SIXES"), DICE_VALUES, DICE_VALUES, DICE_VALUES, DICE_VALUES, DICE_VALUES);
     }
 
     @ParameterizedTest
-    @MethodSource("yatzyProvider")
-    void pair(YatzyCalculator yatzy2) {
-        assertEquals(6, yatzy2.score(List.of(3,4,3,5,6), "PAIR"));
-        assertEquals(10, yatzy2.score(List.of(5,3,3,3,5), "PAIR"));
-        assertEquals(12, yatzy2.score(List.of(5,3,6,6,5), "PAIR"));
+    @FieldSource("yatzyCalculators")
+    void pair(YatzyCalculator calculator) {
+        verifyAllCombinations(scoreOfCategory(calculator, "PAIR"), DICE_VALUES, DICE_VALUES, DICE_VALUES, DICE_VALUES, DICE_VALUES);
     }
 
     @ParameterizedTest
-    @MethodSource("yatzyProvider")
-    void two_pair(YatzyCalculator yatzy2) {
-        assertEquals(16, yatzy2.score(List.of(3,3,5,4,5), "TWO_PAIRS"));
-        assertEquals(16, yatzy2.score(List.of(3,3,5,5,5), "TWO_PAIRS"));
+    @FieldSource("yatzyCalculators")
+    void two_pair(YatzyCalculator calculator) {
+        verifyAllCombinations(scoreOfCategory(calculator, "TWO_PAIRS"), DICE_VALUES, DICE_VALUES, DICE_VALUES, DICE_VALUES, DICE_VALUES);
     }
 
     @ParameterizedTest
-    @MethodSource("yatzyProvider")
-    void three_of_a_kind(YatzyCalculator yatzy2)
-    {
-        assertEquals(9, yatzy2.score(List.of(3,3,3,4,5), "THREE_OF_A_KIND"));
-        assertEquals(15, yatzy2.score(List.of(5,3,5,4,5), "THREE_OF_A_KIND"));
-        assertEquals(9, yatzy2.score(List.of(3,3,3,3,5), "THREE_OF_A_KIND"));
+    @FieldSource("yatzyCalculators")
+    void three_of_a_kind(YatzyCalculator calculator) {
+        verifyAllCombinations(scoreOfCategory(calculator, "THREE_OF_A_KIND"), DICE_VALUES, DICE_VALUES, DICE_VALUES, DICE_VALUES, DICE_VALUES);
     }
 
     @ParameterizedTest
-    @MethodSource("yatzyProvider")
-    void four_of_a_knd(YatzyCalculator yatzy2) {
-        assertEquals(12, yatzy2.score(List.of(3,3,3,3,5), "FOUR_OF_A_KIND"));
-        assertEquals(20, yatzy2.score(List.of(5,5,5,4,5), "FOUR_OF_A_KIND"));
-        assertEquals(12, yatzy2.score(List.of(3,3,3,3,3), "FOUR_OF_A_KIND"));
+    @FieldSource("yatzyCalculators")
+    void four_of_a_knd(YatzyCalculator calculator) {
+        verifyAllCombinations(scoreOfCategory(calculator, "FOUR_OF_A_KIND"), DICE_VALUES, DICE_VALUES, DICE_VALUES, DICE_VALUES, DICE_VALUES);
     }
 
     @ParameterizedTest
-    @MethodSource("yatzyProvider")
-    void smallStraight(YatzyCalculator yatzy2) {
-        assertEquals(15, yatzy2.score(List.of(1,2,3,4,5), "SMALL_STRAIGHT"));
-        assertEquals(15, yatzy2.score(List.of(2,3,4,5,1), "SMALL_STRAIGHT"));
-        assertEquals(0, yatzy2.score(List.of(1,2,2,4,5), "SMALL_STRAIGHT"));
+    @FieldSource("yatzyCalculators")
+    void smallStraight(YatzyCalculator calculator) {
+        // Yatzy calculators have divergent results, so their expected results need to be distinct
+        Options approvalsOptions = Approvals.NAMES.withParameters(calculator.getClass().getSimpleName());
+
+        verifyAllCombinations(scoreOfCategory(calculator, "SMALL_STRAIGHT"), DICE_VALUES, DICE_VALUES, DICE_VALUES, DICE_VALUES, DICE_VALUES, approvalsOptions);
     }
 
     @ParameterizedTest
-    @MethodSource("yatzyProvider")
-    void largeStraight(YatzyCalculator yatzy2) {
-        assertEquals(20, yatzy2.score(List.of(6,2,3,4,5), "LARGE_STRAIGHT"));
-        assertEquals(20, yatzy2.score(List.of(2,3,4,5,6), "LARGE_STRAIGHT"));
-        assertEquals(0, yatzy2.score(List.of(1,2,2,4,5), "LARGE_STRAIGHT"));
+    @FieldSource("yatzyCalculators")
+    void largeStraight(YatzyCalculator calculator) {
+        // Yatzy calculators have divergent results, so their expected results need to be distinct
+        Options approvalsOptions = Approvals.NAMES.withParameters(calculator.getClass().getSimpleName());
+
+        verifyAllCombinations(scoreOfCategory(calculator, "LARGE_STRAIGHT"), DICE_VALUES, DICE_VALUES, DICE_VALUES, DICE_VALUES, DICE_VALUES, approvalsOptions);
     }
     @ParameterizedTest
-    @MethodSource("yatzyProvider")
-    void fullHouse(YatzyCalculator yatzy2) {
-        assertEquals(18, yatzy2.score(List.of(6,2,2,2,6), "FULL_HOUSE"));
-        assertEquals(0, yatzy2.score(List.of(2,3,4,5,6), "FULL_HOUSE"));
+    @FieldSource("yatzyCalculators")
+    void fullHouse(YatzyCalculator calculator) {
+        verifyAllCombinations(scoreOfCategory(calculator, "FULL_HOUSE"), DICE_VALUES, DICE_VALUES, DICE_VALUES, DICE_VALUES, DICE_VALUES);
+    }
+
+    private static Function5<Integer, Integer, Integer, Integer, Integer, Integer> scoreOfCategory(YatzyCalculator calculator, String category) {
+        return (d1, d2, d3, d4, d5) -> calculator.score(List.of(d1, d2, d3, d4, d5), category);
     }
 
 }
