ee7f78841207aa276fd7e7925e7911b553681aa3
emily <emily@bacheconsulting.com>
2025-12-11 09:59:00 +0100

    sample test names in python

---
+++ python/tests/test_sample_names.py
@@ -0,0 +1,25 @@
+from yatzy1 import Yatzy
+
+
+def test_1():
+    assert 5 == Yatzy.chance(1, 1, 1, 1, 1)
+
+
+def test_chance():
+    assert 5 == Yatzy.chance(1, 1, 1, 1, 1)
+
+
+def test_chance_sums_the_dice():
+    assert 5 == Yatzy.chance(1, 1, 1, 1, 1)
+
+
+def test_score_chance_sums_dice():
+    assert 5 == Yatzy.chance(1, 1, 1, 1, 1)
+
+
+def test_given_all_ones_score_chance_returns_5():
+    assert 5 == Yatzy.chance(1, 1, 1, 1, 1)
+
+
+def test_score__any_dice_on_chance__gives_sum():
+    assert 5 == Yatzy.chance(1, 1, 1, 1, 1)
