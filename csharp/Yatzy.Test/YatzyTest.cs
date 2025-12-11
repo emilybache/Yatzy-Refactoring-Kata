1e387490abd54d159a83ffe5e9e761d730876f5e
emily <emily@bacheconsulting.com>
2025-12-11 09:58:16 +0100

    update csharp version

---
+++ csharp/Yatzy.Test/YatzyTest.cs
@@ -0,0 +1,49 @@
+using Xunit;
+
+namespace Yatzy.Test;
+/*
+ * Example test names for review
+ */
+public class YatzyTest
+{
+    [Fact]
+    public void test1()
+    {
+        Assert.Equal(15, Yatzy1.Yatzy1.Chance(2, 3, 4, 5, 1));
+        Assert.Equal(16, Yatzy1.Yatzy1.Chance(3, 3, 4, 5, 1));
+    }    
+    
+    [Fact]
+    public void TestChance()
+    {
+        Assert.Equal(15, Yatzy1.Yatzy1.Chance(2, 3, 4, 5, 1));
+        Assert.Equal(16, Yatzy1.Yatzy1.Chance(3, 3, 4, 5, 1));
+    }    
+    
+    [Fact]
+    public void ChanceSumsTheDice()
+    {
+        Assert.Equal(15, Yatzy1.Yatzy1.Chance(2, 3, 4, 5, 1));
+        Assert.Equal(16, Yatzy1.Yatzy1.Chance(3, 3, 4, 5, 1));
+    }    
+    
+    [Fact]
+    public void Chance_Sums_The_Dice()
+    {
+        Assert.Equal(15, Yatzy1.Yatzy1.Chance(2, 3, 4, 5, 1));
+        Assert.Equal(16, Yatzy1.Yatzy1.Chance(3, 3, 4, 5, 1));
+    }   
+    
+    [Fact]
+    public void Given_All_Ones_Score_Chance_Returns_5()
+    {
+        Assert.Equal(5, Yatzy1.Yatzy1.Chance(1,1,1,1,1));
+    }
+        
+    [Fact]
+    public void Any_Dice_Score_Chance_Gives_Sum()
+    {
+        Assert.Equal(15, Yatzy1.Yatzy1.Chance(2, 3, 4, 5, 1));
+        Assert.Equal(16, Yatzy1.Yatzy1.Chance(3, 3, 4, 5, 1));
+    } 
+}
